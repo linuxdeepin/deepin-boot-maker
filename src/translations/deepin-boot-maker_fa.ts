6948bc049c8db2f9ef0590c78ee059433217e67c
transifex <transifex@linuxdeepin.com>
2017-04-14 06:37:44 +0000

    auto sync po files from transifex
    Change-Id: Ibbb677513e44a9dc5abdd9b8ec0f1b6a7db822af

--- src/translations/deepin-boot-maker_fa.ts
+++ src/translations/deepin-boot-maker_fa.ts
@@ -1,35 +1,35 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="zh_CN" version="2.1">
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="fa" version="2.1">
 <context>
     <name>BMHandler</name>
     <message>
         <location filename="../libdbm/backend/bootmaker.cpp" line="43"/>
         <source>Failed to call the command %1.</source>
-        <translation>执行命令 %1 失败。</translation>
+        <translation>ناموفقیت در اجرای دستور %1</translation>
     </message>
     <message>
         <location filename="../libdbm/backend/bootmaker.cpp" line="45"/>
         <source>Disk Format Error: Please format the disk with FAT32</source>
-        <translation>磁盘格式错误：请使用FAT32格式重新格式化磁盘</translation>
+        <translation>خطا فرمت دیسک: لطفا دیسک را بر FAT32 فرمت کنید</translation>
     </message>
     <message>
         <location filename="../libdbm/backend/bootmaker.cpp" line="47"/>
         <source>Insufficient Disk Space: Please ensure that the disk has %1 free space</source>
-        <translation>磁盘空间不足：请确保至少有%1的剩余空间</translation>
+        <translation>فضای ناکافی دیسک: لطفا مطمئن شوید که دیسک %1 فضای خالی دارد </translation>
     </message>
     <message>
         <location filename="../libdbm/backend/bootmaker.cpp" line="49"/>
         <source>Disk Mount Error: Please plug in the disk again or reboot the system to retry</source>
-        <translation>磁盘挂载错误：请重新插入磁盘或重启系统后重试</translation>
+        <translation>خطا دسترسی به دیسک: لطفا برای تلاش مجدد ، دوباره دیسک را وصل کنید یا سیستم را دوباره راه اندازی کنید </translation>
     </message>
     <message>
         <location filename="../libdbm/backend/bootmaker.cpp" line="51"/>
         <source>Image Uncompress Error: Please check the md5 checksum of the image and ensure the image is complete</source>
-        <translation>解压镜像失败：请检查镜像文件的md5, 确保镜像文件完整</translation>
+        <translation>خطا گستردن ایمیج: لطفا MD5 CHECKSUM فایل ایمیج را چک کنید و مطمئن شوید که فایل ایمیج کامل است</translation>
     </message>
     <message>
         <location filename="../libdbm/backend/bootmaker.cpp" line="53"/>
         <source>Internal Error</source>
-        <translation>未知错误</translation>
+        <translation>خطای داخلی </translation>
     </message>
 </context>
 <context>
@@ -37,7 +37,7 @@
     <message>
         <location filename="../app/main.cpp" line="37"/>
         <source>Deepin Boot Maker</source>
-        <translation>启动盘制作工具</translation>
+        <translation>سازنده بوت لینوکس دیپین</translation>
     </message>
 </context>
 <context>
@@ -45,32 +45,32 @@
     <message>
         <location filename="../app/view/isoselectview.cpp" line="25"/>
         <source>Please select an ISO image file</source>
-        <translation>请选择光盘镜像文件</translation>
+        <translation>لطفا یک فایل ISO انتخاب کنید</translation>
     </message>
     <message>
         <location filename="../app/view/isoselectview.cpp" line="47"/>
         <source>Drag an ISO image file and drop it here</source>
-        <translation>拖拽光盘镜像文件到此</translation>
+        <translation>یک فایل ISO بگیرید و اینجا رها کنید</translation>
     </message>
     <message>
         <location filename="../app/view/isoselectview.cpp" line="51"/>
         <source>OR</source>
-        <translation>或者</translation>
+        <translation>یا</translation>
     </message>
     <message>
         <location filename="../app/view/isoselectview.cpp" line="63"/>
         <source>Select an ISO image file</source>
-        <translation>选择光盘镜像文件</translation>
+        <translation>یک فایل ISO انتخاب کنید</translation>
     </message>
     <message>
         <location filename="../app/view/isoselectview.cpp" line="81"/>
         <source>Next</source>
-        <translation>下一步</translation>
+        <translation>بعدی</translation>
     </message>
     <message>
         <location filename="../app/view/isoselectview.cpp" line="135"/>
         <source>Reselect an ISO image file</source>
-        <translation>重新选择光盘镜像文件</translation>
+        <translation>لطفا دوباره یک فایل ISO انتخاب کنید</translation>
     </message>
 </context>
 <context>
@@ -78,22 +78,22 @@
     <message>
         <location filename="../app/view/progressview.cpp" line="23"/>
         <source>Making</source>
-        <translation>正在制作</translation>
+        <translation>فرایند ساختن</translation>
     </message>
     <message>
         <location filename="../app/view/progressview.cpp" line="29"/>
         <source>Making the disk, please wait...</source>
-        <translation>正在制作，请稍后...</translation>
+        <translation>فرایند ساختن دیسک ، لطفا صبر کنید..</translation>
     </message>
     <message>
         <location filename="../app/view/progressview.cpp" line="32"/>
         <source>Please do not remove the disk or shut down the computer when making</source>
-        <translation>制作时请不要移除磁盘或关机</translation>
+        <translation>لطفا در هنگام فرایند ، دیسک را خارج نکنید یا کامپیوتر را خاموش نکنید </translation>
     </message>
     <message>
         <location filename="../app/view/progressview.cpp" line="40"/>
         <source>Cancel</source>
-        <translation>取消</translation>
+        <translation>انصراف</translation>
     </message>
 </context>
 <context>
@@ -101,12 +101,12 @@
     <message>
         <location filename="../libdbm/util/deviceinfo.h" line="27"/>
         <source>Removable Disk</source>
-        <translation>可移动磁盘</translation>
+        <translation>درایو جدا شونده</translation>
     </message>
     <message>
         <location filename="../libdbm/util/utils.cpp" line="95"/>
         <source>Removable disk</source>
-        <translation>可移动磁盘</translation>
+        <translation>درایو جدا شونده</translation>
     </message>
 </context>
 <context>
@@ -114,47 +114,47 @@
     <message>
         <location filename="../app/view/resultview.cpp" line="58"/>
         <source>Reboot later</source>
-        <translation>稍后重启</translation>
+        <translation>بعدا راه اندازی شود </translation>
     </message>
     <message>
         <location filename="../app/view/resultview.cpp" line="65"/>
         <source>Reboot now</source>
-        <translation>立即重启</translation>
+        <translation>هم اکنون راه اندازی مجدد شود</translation>
     </message>
     <message>
         <location filename="../app/view/resultview.cpp" line="31"/>
         <source>Make Successfully</source>
-        <translation>制作成功</translation>
+        <translation>تولید با موفقیت انجام شد </translation>
     </message>
     <message>
         <location filename="../app/view/resultview.cpp" line="39"/>
         <source>Make successfully, reboot to experience</source>
-        <translation>制作成功，重启即刻体验</translation>
+        <translation>تولید با موفقیت انجام شد ،برای تجربه کردن دوباره راه اندازی کنید</translation>
     </message>
     <message>
         <location filename="../app/view/resultview.cpp" line="100"/>
         <source>The error log will be uploaded automatically with feedback. We can not improve without your feedback and support</source>
-        <translation>反馈问题时会自动上传错误日志，我们的进步离不开您的反馈和支持</translation>
+        <translation>لیست خطاها به همراه بازخورد ، خودکار آپلود خواهد شد . بدون حمایت و بازخورد شما ما نمی توانیم بهتر شویم </translation>
     </message>
     <message>
         <location filename="../app/view/resultview.cpp" line="101"/>
         <source>Feedback</source>
-        <translation>反馈问题</translation>
+        <translation>بازخورد</translation>
     </message>
     <message>
         <location filename="../app/view/resultview.cpp" line="115"/>
         <source>Close</source>
-        <translation>关闭</translation>
+        <translation>بستن</translation>
     </message>
     <message>
         <location filename="../app/view/resultview.cpp" line="123"/>
         <source>Sorry, making failed</source>
-        <translation>制作失败，非常抱歉</translation>
+        <translation>افسوس ، تولید ناموفق بود </translation>
     </message>
     <message>
         <location filename="../app/view/resultview.cpp" line="124"/>
         <source>Making failed</source>
-        <translation>制作失败</translation>
+        <translation>فرایندتولید ناموفق بود</translation>
     </message>
 </context>
 <context>
@@ -162,27 +162,27 @@
     <message>
         <location filename="../app/view/usbselectview.cpp" line="59"/>
         <source>Please select a disk</source>
-        <translation>请选择磁盘</translation>
+        <translation>لطفا یک دیسک را انتخاب کنید </translation>
     </message>
     <message>
         <location filename="../app/view/usbselectview.cpp" line="76"/>
         <source>Formating disk can increase the making success rate</source>
-        <translation>格式化磁盘可提高制作成功率</translation>
+        <translation>فرمت کردن دیسک می تواند احتمال موفقیت در فرایند تولید را افزایش دهد  </translation>
     </message>
     <message>
         <location filename="../app/view/usbselectview.cpp" line="90"/>
         <source>No available disk found</source>
-        <translation>未发现可用磁盘</translation>
+        <translation>هیچ دیسک در دسترسی یافت نشد </translation>
     </message>
     <message>
         <location filename="../app/view/usbselectview.cpp" line="102"/>
         <source>Start making</source>
-        <translation>开始制作</translation>
+        <translation>شروع ساخت</translation>
     </message>
     <message>
         <location filename="../app/view/usbselectview.cpp" line="119"/>
         <source>The disk data will be completely  deleted by formating, please confirm and continue</source>
-        <translation>格式化将彻底删除磁盘上的数据，请您确认后继续</translation>
+        <translation>اطلاعات دیسک تماما با فرمت کردن پاک خواهدشد،لطفا تصدیق کنید و ادامه دهید </translation>
     </message>
 </context>
 </TS>
