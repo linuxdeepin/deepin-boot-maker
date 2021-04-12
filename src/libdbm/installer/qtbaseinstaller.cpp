cd48b971799d8e9873add0fa19244d15892a0ed3
ut000209 <shenfusheng@uniontech.com>
2021-04-12 10:37:34 +0800

    fix: 添加版权注释
    添加版权注释
    
    Log: 添加版权注释
    Change-Id: I5e3cf44643589edaf9a8cd33372a4b63ebf52dee

--- src/libdbm/installer/qtbaseinstaller.cpp
+++ src/libdbm/installer/qtbaseinstaller.cpp
@@ -1,3 +1,24 @@
+/*
+ * Copyright (C) 2020 ~ 2021 Uniontech Software Co., Ltd.
+ *
+ * Author:     shenfusheng <shenfusheng@uniontech.com>
+ *
+ * Maintainer: shenfusheng <shenfusheng@uniontech.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "qtbaseinstaller.h"
 #include "../util/utils.h"
 
