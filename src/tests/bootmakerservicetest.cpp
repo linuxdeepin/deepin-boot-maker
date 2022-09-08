f5f16ba3298ee9d754f000c442f99f5a0f06676c
dutianxue <dutianxue@uniontech.com>
2022-09-08 16:11:12 +0800

    chore: adapt license and copyright Modify project to pass the REUSE check
    Log: Modify project to pass the REUSE check
    Task: https://pms.uniontech.com/task-view-187933.html

--- src/tests/bootmakerservicetest.cpp
+++ src/tests/bootmakerservicetest.cpp
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2022 UnionTech Software Technology Co., Ltd.
+//
+// SPDX-License-Identifier: GPL-3.0-only
+
 #include "bootmakerservicetest.h"
 #include <QDebug>
 // Sets up the test fixture.
--- src/tests/test-recoverage.sh
+++ src/tests/test-recoverage.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# SPDX-FileCopyrightText: 2022 UnionTech Software Technology Co., Ltd.
+#
+# SPDX-License-Identifier: GPL-3.0-only
+
 BUILD_DIR=build
 REPORT_DIR=report
 
